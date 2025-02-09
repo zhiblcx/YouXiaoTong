7195397981c13f3b5d18fde47b60f541eb62873f
zhiblcx <1529088692@qq.com>
2025-02-09 16:04:49 +0800

    feat(client): add article page

---
+++ apps/client/pages/article/[id].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="p-2">
+    <div class="text-2xl font-bold">考试考试安排考试考试安排考试安排考安排考试安排考安排</div>
+    <div class="text-[#999] text-right mt-2">2025/02/08 21:13</div>
+  </div>
+  <div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+  </div>
+</template>
+
+<style scoped></style>
