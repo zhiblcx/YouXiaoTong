7c0c0a3ce14040a0bc966fdd31aa32c00c198623
zhiblcx <1529088692@qq.com>
2025-02-10 12:09:56 +0800

    feat(client): add address

---
+++ apps/client/pages/setting/addressContent/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const route = useRoute()
+if (route.query.id === undefined) {
+  console.log('新增')
+} else {
+  console.log('修改')
+}
+
+function onSave() {
+  showSuccessToast('保存成功')
+}
+
+function onDelete() {
+  showSuccessToast('删除成功')
+}
+
+function changeDefault() {
+  showSuccessToast('切换默认地址')
+}
+</script>
+
+<template>
+  <van-address-edit
+    :show-area="false"
+    show-delete
+    show-set-default
+    @change-default="changeDefault"
+    @save="onSave"
+    @delete="onDelete"
+  />
+</template>
