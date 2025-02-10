aa2acc0eaa2a66a1ffbd7dd8f74221572aa9151f
zhiblcx <1529088692@qq.com>
2025-02-10 12:10:55 +0800

    feat(client): add address list page

---
+++ apps/client/pages/setting/AddressList.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+const router = useRouter()
+const chosenAddressId = ref('1')
+const list = [
+  {
+    id: '1',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '2',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  },
+  {
+    id: '3',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '4',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  },
+  {
+    id: '5',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '6',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  },
+  {
+    id: '7',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '8',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  },
+  {
+    id: '9',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '10',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  },
+  {
+    id: '11',
+    name: '张三',
+    tel: '13000000000',
+    address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室',
+    isDefault: true
+  },
+  {
+    id: '12',
+    name: '李四',
+    tel: '1310000000',
+    address: '浙江省杭州市拱墅区莫干山路 50 号'
+  }
+]
+
+const onAdd = () => {
+  showToast('新增地址')
+  router.push('/setting/addresscontent')
+}
+const onEdit = (item, index) => {
+  showToast('编辑地址:' + index)
+  router.push('/setting/addresscontent?id=1')
+}
+</script>
+
+<template>
+  <van-address-list
+    v-model="chosenAddressId"
+    :list="list"
+    @add="onAdd"
+    @edit="onEdit"
+  />
+</template>
