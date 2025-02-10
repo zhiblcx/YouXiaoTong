8ca4be20b3a0f73e5eb417a1d8c4fae3a1fe05d5
zhiblcx <1529088692@qq.com>
2025-02-10 12:10:05 +0800

    feat(client): add person info page

---
+++ apps/client/pages/setting/PersonInfo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import PersonInfoImg from '@/assets/images/person-info.png'
+</script>
+
+<template>
+  <img
+    class="rounded-lg absolute top-[-30px]"
+    :src="PersonInfoImg"
+  />
+  <van-cell-group class="mt-[250px]">
+    <van-cell
+      title="学号"
+      value="20250209"
+    />
+    <van-cell
+      title="姓名"
+      value="张三"
+    />
+    <van-cell
+      title="性别"
+      value="男"
+    />
+    <van-cell
+      title="年龄"
+      value="18"
+    />
+    <van-cell
+      title="宿舍"
+      value="15栋405"
+    />
+  </van-cell-group>
+</template>
