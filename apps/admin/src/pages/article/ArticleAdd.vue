1f23942a863bae8a1d4d7be9359385bd5001fd7a
zhiblcx <1529088692@qq.com>
2025-02-15 12:37:19 +0800

    feat(admin): complete article page

--- apps/admin/src/pages/article/ArticleAdd.vue
+++ apps/admin/src/pages/article/ArticleAdd.vue
@@ -1,90 +1,11 @@
-<template>
-  <div style="display: flex; align-items: end; justify-content: space-between; margin-bottom: 5px">
-    <div>
-      <div style="margin-bottom: 20px">
-        <span>文章标题：</span>
-        <a-input
-          v-model:value="article.title"
-          placeholder="请输入标题"
-          style="width: 300px"
-        />
-      </div>
-      <div style="margin-bottom: 20px">
-        <span>文章类型：</span>
-        <a-select
-          v-model:value="type"
-          label-in-value
-          style="width: 300px"
-          :options="options"
-          @change="handleChangeType"
-        >
-        </a-select>
-      </div>
-      <a-form-item
-        label="文章图片"
-        name="avator"
-      >
-        <a-upload
-          v-model:file-list="fileList"
-          name="file"
-          list-type="picture-card"
-          class="avatar-uploader"
-          :show-upload-list="false"
-          action="/api/users/updatepicture?address=article"
-          :before-upload="beforeUpload"
-          @change="handleChange"
-        >
-          <img
-            v-if="imageUrl"
-            :src="imageUrl"
-            alt="avatar"
-            style="width: 100px"
-          />
-          <div v-else>
-            <loading-outlined v-if="loading" />
-            <plus-outlined v-else />
-            <div class="ant-upload-text">upload</div>
-          </div>
-        </a-upload>
-      </a-form-item>
-    </div>
-    <a-button
-      type="primary"
-      style="margin-right: 10px"
-      @click="uploadArticle"
-      v-if="article.edit_timer == undefined"
-      >上传文章</a-button
-    >
-    <a-button
-      type="primary"
-      style="margin-right: 10px"
-      @click="editArticle"
-      v-else
-      >修改文章</a-button
-    >
-  </div>
-  <div>
-    <div style="margin-bottom: 10px">文章详情：</div>
-    <!-- 此处注意写法v-model:content -->
-    <QuillEditor
-      ref="myQuillEditor"
-      theme="snow"
-      v-model:content="article.detail"
-      :options="data.editorOption"
-      contentType="html"
-      @update:content="setValue()"
-    />
-  </div>
-</template>
-
 <script setup>
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import { message } from 'ant-design-vue'
 import { onMounted, ref, toRaw, watch } from 'vue'
-import { uploadArticleApi, updateArticleApi, showPointArticle } from '../../api'
 import { useRouter, useRoute } from 'vue-router'
-import dayjs from 'dayjs'
+import { addArticleApi } from '@/api'
+import { showArticleByIdApi, updateArticleApi } from '../../api'
 const router = useRouter()
 const route = useRoute()
 const fileList = ref([])
@@ -92,7 +13,7 @@ const myQuillEditor = ref()
 const fileBtn = ref()
 const imageUrl = ref('')
 const loading = ref(false)
-const data = ref({
+const articleData = ref({
   content: '',
   editorOption: {
     modules: {
@@ -112,24 +33,11 @@ const data = ref({
   }
 })
 
-const options = ref([
-  { value: '安全与健康', label: '安全与健康' },
-  { value: '人际交往与适应', label: '人际交往与适应' },
-  { value: '生活与习惯', label: '生活与习惯' },
-  { value: '知识与智力', label: '知识与智力' },
-  { value: '亲子沟通', label: '亲子沟通' }
-])
-const type = ref(options.value[0])
-
 const article = ref({
   title: '',
-  detail: '',
-  picture: ''
+  content: ''
 })
 
-const handleChangeType = (value) => {
-  console.log(value) // { key: "lucy", label: "Lucy (101)" }
-}
 watch(
   () => (val) => {
     toRaw(myQuillEditor.value).setHTML(val)
@@ -143,35 +51,38 @@ onMounted(async () => {
   if (myQuillEditor.value) {
     quill.getModule('toolbar').addHandler('image', imgHandler)
   }
+
   const { id } = route.query
   if (id != undefined) {
-    const result = await showPointArticle(id)
-    if (result.data.code == 200) {
-      article.value = result.data.data
-      type.value = options.value.filter((item) => item.value == article.value.type)
-      imageUrl.value = 'http://127.0.0.1:3000/' + article.value.picture
-      toRaw(myQuillEditor.value).setHTML(article.value.detail)
+    const { data: result } = await showArticleByIdApi(id)
+    if (result.statusCode === undefined) {
+      article.value = result
+      imageUrl.value = article.value.photo
+      toRaw(myQuillEditor.value).setHTML(article.value.content)
     }
   }
 })
 
 const uploadArticle = async () => {
   const regex = /<[^>]+>([\s\S]*?)<\/[^>]+>/g
-  const matches = article.value.detail.match(regex)
+  const matches = article.value.content.match(regex)
   if (matches == null) {
     message.error('请填写完整')
     return
   }
-  article.value.type = type.value.value
-  if (article.value.title.trim() == '' || article.value.picture == null) {
+
+  if (!article.value.title.trim() || imageUrl.value === null) {
     message.error('请填写完整')
     return
   }
 
-  article.value.edit_timer = dayjs().format('YYYY/MM/DD HH:mm')
-
-  const result = await uploadArticleApi({ ...article.value })
-  if (result.data.code == 200) {
+  const { data } = await addArticleApi({
+    title: article.value.title,
+    status: article.value.status,
+    photo: imageUrl.value,
+    content: article.value.content
+  })
+  if (data.statusCode === undefined) {
     message.success('上传成功')
     setTimeout(() => {
       router.push('/article/editarticle')
@@ -184,19 +95,19 @@ const uploadArticle = async () => {
 async function editArticle() {
   // 提取标签内的内容
   const regex = /<[^>]+>([\s\S]*?)<\/[^>]+>/g
-  const matches = article.value.detail.match(regex)
+  const matches = article.value.content.match(regex)
   if (matches == null) {
     message.error('请填写完整')
     return
   }
-  article.value.type = type.value.value
-  if (article.value.title.trim() == '' || article.value.picture == null) {
+
+  if (article.value.title.trim() == '' || imageUrl.value == null) {
     message.error('请填写完整')
     return
   }
 
-  const result = await updateArticleApi({ ...article.value })
-  if (result.data.code == 200) {
+  const { data: result } = await updateArticleApi(route.query.id, { ...article.value, photo: imageUrl.value })
+  if (result.statusCode === undefined) {
     message.success('修改成功')
     setTimeout(() => {
       router.push('/article/editarticle')
@@ -211,9 +122,9 @@ const imgHandler = (state) => {
     fileBtn.value.click()
   }
 }
-// 抛出更改内容，此处避免出错直接使用文档提供的getHTML方法
+
 const setValue = () => {
-  const text = toRaw(myQuillEditor.value).getHTML()
+  // const text = toRaw(myQuillEditor.value).getHTML()
 }
 
 function getBase64(img, callback) {
@@ -223,42 +134,102 @@ function getBase64(img, callback) {
 }
 
 const handleChange = (info) => {
-  if (info.file.status === 'uploading') {
-    loading.value = true
-    return
-  }
-  if (info.file.status === 'done') {
-    getBase64(info.file.originFileObj, (base64Url) => {
-      imageUrl.value = base64Url
-      loading.value = false
-    })
-    if (info.file.response.code == 200) {
-      article.value.picture = info.file.response.data
-    }
-  }
-  if (info.file.status === 'error') {
+  getBase64(info.file.originFileObj, (base64Url) => {
+    imageUrl.value = base64Url
     loading.value = false
-    message.error('upload error')
-  }
+  })
 }
 
 const beforeUpload = (file) => {
   const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
   if (!isJpgOrPng) {
-    message.error('You can only upload JPG file!')
+    message.error('你只能上传 JPG 和 PNG 类型的图片')
   }
   const isLt2M = file.size / 1024 / 1024 < 2
   if (!isLt2M) {
-    message.error('Image must smaller than 2MB!')
+    message.error('图像不能超过2MB!')
   }
   return isJpgOrPng && isLt2M
 }
 </script>
+
+<template>
+  <div style="display: flex; align-items: end; justify-content: space-between; margin-bottom: 5px">
+    <div>
+      <div style="margin-bottom: 20px">
+        <span>文章标题：</span>
+        <a-input
+          v-model:value="article.title"
+          placeholder="请输入标题"
+          style="width: 300px"
+        />
+      </div>
+      <a-form-item
+        label="文章图片"
+        name="avator"
+      >
+        <a-upload
+          v-model:file-list="fileList"
+          name="file"
+          list-type="picture-card"
+          class="avatar-uploader"
+          :show-upload-list="false"
+          :before-upload="beforeUpload"
+          @change="handleChange"
+        >
+          <img
+            v-if="imageUrl"
+            :src="imageUrl"
+            alt="avatar"
+            style="width: 100px"
+          />
+          <div v-else>
+            <loading-outlined v-if="loading" />
+            <plus-outlined v-else />
+            <div class="ant-upload-text">upload</div>
+          </div>
+        </a-upload>
+      </a-form-item>
+      <div style="margin-bottom: 20px">
+        <span>文章状态：</span>
+        <a-switch v-model:checked="article.status" />
+      </div>
+    </div>
+    <a-button
+      type="primary"
+      style="margin-right: 10px"
+      @click="uploadArticle"
+      v-if="route.query.id == undefined"
+      >上传文章</a-button
+    >
+    <a-button
+      type="primary"
+      style="margin-right: 10px"
+      @click="editArticle"
+      v-else
+      >修改文章</a-button
+    >
+  </div>
+  <div>
+    <div style="margin-bottom: 10px">文章详情：</div>
+    <!-- 此处注意写法v-model:content -->
+    <QuillEditor
+      ref="myQuillEditor"
+      theme="snow"
+      v-model:content="article.content"
+      :options="articleData.editorOption"
+      contentType="html"
+      @update:content="setValue()"
+    />
+  </div>
+</template>
+
 <style scoped lang="scss">
 // 调整样式
 :deep(.ql-editor) {
   min-height: 400px;
 }
+
 :deep(.ql-formats) {
   height: 21px;
   line-height: 21px;
--- apps/admin/src/pages/article/ArticleEdit.vue
+++ apps/admin/src/pages/article/ArticleEdit.vue
@@ -2,8 +2,8 @@
 import { ref, onMounted } from 'vue'
 import { QuestionCircleOutlined } from '@ant-design/icons-vue'
 import { message } from 'ant-design-vue'
-import { showAllArticle, deleteArticleApi } from '../../api'
 import { useRouter } from 'vue-router'
+import { getArticleApi, deleteArticleApi, updateArticleStatusApi } from '@/api'
 
 const columns = ref([
   {
@@ -17,9 +17,9 @@ const columns = ref([
     key: 'picture'
   },
   {
-    title: '文章类型',
-    dataIndex: 'type',
-    key: 'type'
+    title: '文章状态',
+    dataIndex: 'status',
+    key: 'status'
   },
   {
     title: '文章标题',
@@ -39,37 +39,56 @@ const originData = ref([])
 const router = useRouter()
 
 onMounted(async () => {
-  const result = await showAllArticle()
-  if (result.data.code == 200) {
-    data.value = result.data.data
+  const { data: result } = await getArticleApi()
+  if (result.statusCode == undefined) {
+    data.value = result
     originData.value = [...data.value]
   }
 })
 
 const onSearch = () => {
-  data.value = originData.value.filter(item => {
+  data.value = originData.value.filter((item) => {
     const searchValue = search.value
     const regex = new RegExp(searchValue, 'i')
-    return regex.test(item.type)
+    return regex.test(item.id)
   })
 }
 
 // 删除确认框
-const dismiss = async id => {
-  const result = await deleteArticleApi(id)
-  if (result.data.code == 200) {
-    data.value = data.value.filter(value => value.id != id)
+const dismiss = async (id) => {
+  const { data: result } = await deleteArticleApi(id)
+  if (result.statusCode === undefined) {
+    data.value = data.value.filter((value) => value.id != id)
     message.success('删除成功')
   } else {
     message.error('删除失败')
+  }
+}
+
+const changeStatus = async (record) => {
+  const { data: result } = await updateArticleStatusApi(record.id, record.status)
+  if (result.statusCode === undefined) {
+    message.success('修改状态成功')
+  } else {
+    message.error('修改状态失败')
   }
 }
 </script>
 
 <template>
   <div>
-    <a-input-search v-model:value="search" placeholder="请输入类型" enter-button="搜索" size="large" @search="onSearch" style="margin-bottom: 5px; width: 300px" />
-    <a-table :columns="columns" :data-source="data">
+    <a-input-search
+      v-model:value="search"
+      placeholder="请输入编号"
+      enter-button="搜索"
+      size="large"
+      @search="onSearch"
+      style="margin-bottom: 5px; width: 300px"
+    />
+    <a-table
+      :columns="columns"
+      :data-source="data"
+    >
       <template #headerCell="{ column }">
         <template v-if="column.key === 'id'">
           <span> 文章编号 </span>
@@ -78,14 +97,41 @@ const dismiss = async id => {
 
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'picture'">
-          <a-avatar shape="square" :size="64" :src="'http://127.0.0.1:3000/' + record.picture"> </a-avatar>
+          <a-avatar
+            shape="square"
+            :size="64"
+            :src="record.photo"
+          >
+          </a-avatar>
+        </template>
+
+        <template v-if="column.key === 'status'">
+          <a-switch
+            v-model:checked="record.status"
+            @change="changeStatus(record)"
+          />
         </template>
 
         <template v-else-if="column.key === 'action'">
-          <a-button type="primary" style="margin-left: 20px" @click="router.push(`/article/addarticle?id=${record.id}`)">修改</a-button>
-          <a-popconfirm title="确认删除吗?" ok-text="删除" cancel-text="取消" @confirm="dismiss(record.id)">
+          <a-button
+            type="primary"
+            style="margin-left: 20px"
+            @click="router.push(`/article/addarticle?id=${record.id}`)"
+            >修改</a-button
+          >
+          <a-popconfirm
+            title="确认删除吗?"
+            ok-text="删除"
+            cancel-text="取消"
+            @confirm="dismiss(record.id)"
+          >
             <template #icon><question-circle-outlined style="color: red" /></template>
-            <a-button type="primary" danger style="margin-left: 20px">删除</a-button>
+            <a-button
+              type="primary"
+              danger
+              style="margin-left: 20px"
+              >删除</a-button
+            >
           </a-popconfirm>
         </template>
       </template>
