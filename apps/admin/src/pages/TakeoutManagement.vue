4cbe70d1424b3f6389a42ebb6973b0642c4aafe3
zhiblcx <1529088692@qq.com>
2025-02-17 16:20:12 +0800

    chore(admin): complete TakeoutManagement page

--- apps/admin/src/pages/TakeoutManagement.vue
+++ apps/admin/src/pages/TakeoutManagement.vue
@@ -1,16 +1,30 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { message, notification } from 'ant-design-vue'
+import { showPersonApi } from '@/api'
 
 const text = ref(null)
 const show = ref(false)
+const qrcode = ref({
+  businessId: '',
+  money: 0
+})
+
+onMounted(async () => {
+  const { data: person } = await showPersonApi()
+  if (person.statusCode === undefined) {
+    qrcode.value.businessId = person.id
+  }
+})
 
 watch(text, () => (show.value = false))
 
 const handleShow = () => {
   if (text.value) {
+    qrcode.value.money = text.value
     show.value = true
-    // openNotificationWithIcon()
+    console.log(qrcode.value)
+    // openNotificationWithIcon(text.value)
   } else {
     message.error('请填写交易金额')
   }
@@ -31,14 +45,15 @@ const openNotificationWithIcon = (money) => {
     align="center"
   >
     <a-qrcode
-      :value="text"
+      :value="qrcode"
       :style="{ visibility: show ? 'visible' : 'hidden' }"
     />
-
-    <a-input
+    <a-input-number
       v-model:value="text"
       placeholder="请输入要交易的金额"
-      :maxlength="60"
+      style="width: 200px"
+      addon-after="$"
+      precision="2"
     />
     <a-button
       type="primary"
